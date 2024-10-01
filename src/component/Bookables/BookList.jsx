36ab3fbdcaf786950124662e1ca9b886b4b2546c
samesamessa <cloverremidore@gmail.com>
2024-10-01 18:22:01 +0900

    #3 useState_2 (상태값 관리) 자원의 그룹값 선택할 select 추가

--- src/component/Bookables/BookList.jsx
+++ src/component/Bookables/BookList.jsx
@@ -4,20 +4,26 @@ import {FaArrowRight} from "react-icons/fa";
 
 function BookList(){
     console.log("bookables", bookables)
-    const group = "Rooms"
+    // const group = "Rooms"
+    const [group, setGroup] = useState("Rooms")
     const bookableGroup = bookables.filter(b => (b.group === group))
     // 상태값 관리를 해야할 변수 bookableIndex
     // setBookableIndex 는 useState가 리턴해주는 메소드
     const [bookableIndex, setBookableIndex] = useState(0)
+    const groups = ["Rooms", "Kit"]
 
     function nextBookableIndex() {
         setBookableIndex((i) => (i+1) % bookableGroup.length)
         // 상태값 변경 메소드의 인자 i는 bookableIndex의 값
     }
 
+
     return (
         // return의 최상위태그는 무조건 1개
         <>
+            <select value={group} onChange={(e) => setGroup(e.target.value) }>
+                {groups.map(g => <option key={g} value={g}>{g}</option>)}
+            </select>
             <ul className="items-list-nav">
                 {bookableGroup.map((b, i) => (
                     <li key={b.id} className={i === bookableIndex ? "selected" : null}>
