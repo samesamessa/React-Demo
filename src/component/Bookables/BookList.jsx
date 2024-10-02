70e15088392edfc43fbf9ccb011c0d8f71ac0f91
samesamessa <cloverremidore@gmail.com>
2024-10-02 11:56:54 +0900

    #5 userReducer 주석 추가 BookList 리팩토링

--- src/component/Bookables/BookList.jsx
+++ src/component/Bookables/BookList.jsx
@@ -1,33 +1,75 @@
-import {bookables} from "../../static.json"
-import {useState} from "react";
+import {bookables, days, sessions} from "../../static.json"
+import {useReducer} from "react";
 import {FaArrowRight} from "react-icons/fa";
+import reducer from "./reducer.js";
 
 function BookList(){
-    console.log("bookables", bookables)
-    // const group = "Rooms"
-    const [group, setGroup] = useState("Rooms")
+    // 상태를 관리할 변수를 초기화하는 객체
+    const initState = {
+        group : "Rooms",
+        bookableIndex : 0,
+        hasDetails : false
+    }
+    
+    // state는 상태값들을 모아놓은 오브젝트
+    const [state, dispatch] = useReducer(reducer, initState)
+    // state는 상태값 오브젝트
+    // dispatch 현재상태값 -> 새로운 상태값을 만든다. 이벤트 핸들러에서 실행됨
+    // 위의 2개는 useReducer 함수의 리턴값
+    // reducer state는 현재 상태값. 알아서 가져옴. action은 변경할 값 = payload
+    // useReducer를 생성하는 인자는
+    // reducer는 dispatch가 호출되면 전달한 액션(type, payload)으로 실행할 내용을 정의
+    //              ㄴ useState에서는 이벤트에서 처리하던 동작을 모아서 현재 값을 어떤 수식으로 실행할지 정의
+    // 두번째 인자 initState는 상태값(오브젝트)의 초기 상태
+    // 세번째 인자는 현재 없지만 initState 초기값을 받아 처음 한번 실행하는 초기화 함수
+
+    const {group, bookableIndex, hasDetails} = state
     const bookableGroup = bookables.filter(b => (b.group === group))
-    // 상태값 관리를 해야할 변수 bookableIndex
-    // setBookableIndex 는 useState가 리턴해주는 메소드
-    const [bookableIndex, setBookableIndex] = useState(0)
-    const groups = ["Rooms", "Kit"]
+    const groups = [... new Set(bookables.map(b=>b.group))]
 
     function nextBookableIndex() {
-        setBookableIndex((i) => (i+1) % bookableGroup.length)
-        // 상태값 변경 메소드의 인자 i는 bookableIndex의 값
+        dispatch({
+            type : "NEXT_BOOKABLE",
+            payload : bookableGroup.length
+        })
     }
 
+    function changeGroup(e){
+        console.log("Current hasDetails state:", hasDetails);
+        dispatch({
+            type : "SET_GROUP",
+            payload : e.target.value
+        })
+        console.log("Current hasDetails state:", hasDetails);
+    }
+
+    function changeBookableIndex(selectIndex){
+        dispatch({
+            type : "SET_BOOKABLE",
+            payload : selectIndex
+        })
+    }
+
+    function toggleDetails() {
+        dispatch({
+            type: "TOGGLE_HAS_DETAILS"
+        })
+    }
+
+
+    const bookable = bookableGroup[bookableIndex];
 
     return (
         // return의 최상위태그는 무조건 1개
         <>
-            <select value={group} onChange={(e) => setGroup(e.target.value) }>
+        <div>
+            <select value={group} onChange={changeGroup}>
                 {groups.map(g => <option key={g} value={g}>{g}</option>)}
             </select>
             <ul className="items-list-nav">
                 {bookableGroup.map((b, i) => (
                     <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-                        <button className="btn" onClick={() => setBookableIndex(i)}>
+                        <button className="btn" onClick={() => changeBookableIndex(i)}>
                             {b.title}
                         </button>
                     </li>
@@ -38,6 +80,36 @@ function BookList(){
                     <FaArrowRight/><span>Next</span>
                 </button>
             </p>
+        </div>
+
+            {/* 새로운 UI 추가, CSS 추가 : 상세 내용 */}
+        <div className="book-details">
+            <div className="item">
+                <div className="item-header">
+                    <h2>{bookable.title}</h2>
+                    <span className="controls">
+                        <label>
+                            <input type="checkbox" checked={hasDetails}
+                                   onChange={toggleDetails}/>
+                        </label>
+                    </span>
+                </div>
+                <p>{bookable.notes}</p>
+                {hasDetails && (
+                    <div className="item-details">
+                        <h3>사용 가능한 요일과 세션</h3>
+                        <div className="bookable-availability">
+                            <ul>
+                                {bookable.days.sort().map(d => <li key={d}>{days[d]}</li>)}
+                            </ul>
+                            <ul>
+                                {bookable.sessions.sort().map(s => <li key={s}>{sessions[s]}</li>)}
+                            </ul>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
         </>
     )
 }
---
+++ src/component/Bookables/reducer.js
@@ -0,0 +1,39 @@
+// state는 상태값 오브젝트(state, payload), action은 무엇을 어떻게 처리할 것인가 전달
+//          리턴은 새로운 상태값
+export default function reducer(state, action){
+    switch (action.type){
+        case "SET_GROUP":
+            return{
+                ...state,                       // 기존 state 값을 복사
+                group:action.payload,           // 그룹을 payload 값으로 변경
+                bookableIndex: 0,                // 인덱스를 0으로 초기화
+                hasDetails : false
+            }
+
+        case "SET_BOOKABLE":
+            return{
+                ...state,                       // 기존 state 값 복사
+                bookableIndex: action.payload   // 인덱스를 payload 값으로 변경
+            }
+
+        case "TOGGLE_HAS_DETAILS":
+            return{
+                ...state,                       // 기존 state값 복사
+                hasDetails: !state.hasDetails   // hasDetails을 반대값으로 변경 (true<->false)
+            }
+
+        case "NEXT_BOOKABLE":
+        {
+            // const count = state.bookables.filter(
+            // b=> b.group === state.group).length;
+            return{
+                ...state,                                                   // 기존 state값 복사
+                bookableIndex: (state.bookableIndex +1) % action.payload    // 인덱스를 다음 인덱스 값으로
+            };
+        }
+
+        default:    //꼭 필요함.
+            return state;
+
+    }
+}
